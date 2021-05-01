fe3f64a0fbcbbfba6bb60f6a7892eb8717699b05
vaibhavd2103 <bountydange@gmail.com>
2021-05-01 14:18:46 +0530

    Initialized empty chat app

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
